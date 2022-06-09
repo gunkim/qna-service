a15f3187c933ac613feb527cdfd962e5623f3077
gunkim <gunkim.dev@gmail.com>
2022-06-10 02:45:15 +0900

    :white_check_mark: 테스트 데이터 로더 구현

---
+++ application/src/main/kotlin/io/github/gunkim/DataInitLoader.kt
@@ -0,0 +1,26 @@
+package io.github.gunkim
+
+import io.github.gunkim.question.adapter.out.persistence.*
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class DataInitLoader(
+    private val questionJpaEntityRepository: QuestionJpaEntityRepository
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        val entities = (1..30).map(::createQuestion)
+        questionJpaEntityRepository.saveAll(entities)
+    }
+
+    private fun createQuestion(id: Int): QuestionJpaEntity = QuestionJpaEntity(
+        content = "궁금한게 있어요!! $id",
+        categoryJpaEntity = CategoryJpaEntity(name = "Java"),
+        tagJpaEntities = mutableSetOf(TagJpaEntity(name = "궁금한거")),
+        userEmbedded = UserEmbedded(
+            username = "gunkim$id",
+            password = "testtest$id",
+            ip = "127.0.0.1"
+        )
+    )
+}
