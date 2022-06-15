e5aea9509191959d35807ee1cf7c86d2e572a1b8
shirohoo <shirohoo4102@gmail.com>
2022-06-15 10:41:04 +0900

    :recycle: 도메인
    # Class layout convention
    The contents of a class should go in the following order:
      - Property declarations and initializer blocks
      - Secondary constructors
      - Method declarations
      - Companion object
    
    https://kotlinlang.org/docs/coding-conventions.html#class-layout

--- domain/src/main/kotlin/io/github/gunkim/question/domain/Answer.kt
+++ domain/src/main/kotlin/io/github/gunkim/question/domain/Answer.kt
@@ -3,39 +3,28 @@ package io.github.gunkim.question.domain
 import io.github.gunkim.function.hashCodeOf
 import java.time.LocalDateTime
 
-class Answer private constructor(
-    val id: Long?,
+class Answer(
+    val id: Long? = null,
     val content: String,
     val user: User,
-    val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime?
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime? = null
 ) {
-    companion object {
-        operator fun invoke(
-            id: Long? = null,
-            content: String,
-            user: User,
-            createdAt: LocalDateTime = LocalDateTime.now(),
-            updatedAt: LocalDateTime? = null
-        ): Answer {
-            return Answer(
-                id = id,
-                content = content.takeIf { it.length in 2..1_500 }
-                    ?: throw IllegalArgumentException("답변 내용은 2자 이상 1500자 미만이어야 합니다."),
-                user = user,
-                createdAt = createdAt,
-                updatedAt = updatedAt
-            )
+    init {
+        require(content.length in 2..1_500) {
+            "답변 내용은 2자 이상 1500자 이하여야 합니다."
         }
     }
 
-    override fun equals(other: Any?): Boolean = this === other ||
+    override fun equals(other: Any?): Boolean {
+        return this === other ||
             other is Answer &&
             id == other.id &&
             content == other.content &&
             user == other.user &&
             createdAt == other.createdAt &&
             updatedAt == other.updatedAt
+    }
 
     override fun hashCode(): Int = hashCodeOf(id, content, user, createdAt, updatedAt)
 }
